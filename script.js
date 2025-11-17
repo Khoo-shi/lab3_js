010f32377264caf57c3578ead40905b423b8557f
khoo_shi <Khushdeepk1706@gmail.com>
2025-11-16 21:04:52 -0500

    Added constructor with OOP structure

---
+++ script.js
@@ -0,0 +1,29 @@
+//  Constructor (OOP)
+function ToyCar(name, scale, color, price, length, width, height, material, brand, year, features) {
+    this.name = name;
+    this.scale = scale;
+    this.color = color;
+    this.price = price;
+    this.length = length;
+    this.width = width;
+    this.height = height;
+    this.material = material;
+    this.brand = brand;
+    this.year = year;
+    this.features = features; // array
+
+    // Method: return formatted information
+    this.getInfo = function() {
+        return `
+            <strong>${this.name}</strong><br>
+            Brand: ${this.brand}<br>
+            Scale: ${this.scale}<br>
+            Color: ${this.color}<br>
+            Price: $${this.price}<br>
+            Dimensions: ${this.length} x ${this.width} x ${this.height} cm<br>
+            Material: ${this.material}<br>
+            Year: ${this.year}<br>
+            Features: ${this.features.join(", ")}
+        `;
+    };
+}
